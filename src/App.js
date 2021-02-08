61c106ab715c75401ed3e9613a0be7045037b166
fatma-douiri <76583464+fatma-douiri@users.noreply.github.com>
2021-02-08 13:16:25 +0100

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React , {useState} from "react"
 import './App.css';
+import {MoviesData} from './MoviesData'
+import MoviesList from './Components/MoviesList/MoviesList'
+import Header from './Components/Header/Header'
+import MovieDescription from "./Components/MovieDescreption/MovieDescription";
+import { Route,Switch } from "react-router-dom";
+
 
 function App() {
+  const [movies, setMovies] = useState(MoviesData)
+  const [searchName, setSearchName] = useState("");
+  const [searchRate, setSearchRate] = useState(0);
+
+  const AddNewMovie = (x) => {
+    setMovies([...movies, x]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Switch>
+      <Route exact path='/'>
+        <Header 
+            setSearchName={setSearchName}
+            setSearchRate={setSearchRate}
+            searchRate={searchRate}/>
+        <MoviesList
+            movies={
+               (searchName || searchRate)? movies.filter((el) => el.name.toLowerCase().includes(searchName.toLowerCase().trim())  && el.rating >= searchRate ): movies
+                   }   
+            AddNewMovie={AddNewMovie}/>
+         </Route>   
+      <Route  path='/movies/:id'  render={(props)=> <MovieDescription {...props} movies={movies} />}  />
+   </Switch>
+   
+  
     </div>
   );
 }
